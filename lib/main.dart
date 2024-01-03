05caf252699e61d25643a7973ee78770e0ce905c
Gayun00 <gayunlee11@gmail.com>
2024-01-03 22:03:42 +0900

    Modify: pomodoro timer UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/widgets/buttons.dart';
+import 'package:toonflix/screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,60 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      title: 'Flutter Demo',
+      theme: ThemeData(
           useMaterial3: true,
-        ),
-        home: const Scaffold(
-            body: Column(
-          children: [
-            SizedBox(
-              height: 80,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Column(
-                  children: [
-                    Text(
-                      'hi',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 48,
-                          fontWeight: FontWeight.w300),
-                    ),
-                    Text('sdf',
-                        style: TextStyle(color: Color.fromRGBO(5, 5, 5, 0.8)))
-                  ],
-                )
-              ],
-            ),
-            SizedBox(height: 120),
-            Text('Total balance',
-                style: TextStyle(color: Color.fromRGBO(11, 11, 11, 0.8))),
-            SizedBox(height: 10),
-            Text('\$3 24 33',
-                style: TextStyle(
-                    color: Color.fromRGBO(11, 11, 11, 0.8), fontSize: 44)),
-            SizedBox(height: 20),
-            Row(
-              children: [
-                Button(
-                    text: 'Transfer',
-                    bgColor: Colors.amber,
-                    textColor: Colors.black),
-              ],
-            ),
-            Row(
-              children: [
-                Button(
-                    text: 'Transfer',
-                    bgColor: Colors.amber,
-                    textColor: Colors.black),
-              ],
-            ),
-          ],
-        )));
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)
+              .copyWith(background: Colors.amber)),
+      home: const HomeScreen(),
+    );
   }
 }
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: Column(
+          children: [
+            Flexible(
+                flex: 1,
+                child: Container(
+                    alignment: Alignment.bottomCenter,
+                    child: Text('25:00',
+                        style: TextStyle(
+                            color: Theme.of(context).cardColor,
+                            fontSize: 89,
+                            fontWeight: FontWeight.w600)))),
+            Flexible(
+                flex: 3,
+                child: Center(
+                    child: IconButton(
+                        iconSize: 150,
+                        color: Theme.of(context).cardColor,
+                        onPressed: () {},
+                        icon: const Icon(Icons.play_circle_outline)))),
+            Flexible(
+                flex: 1,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                          decoration:
+                              BoxDecoration(color: Theme.of(context).cardColor),
+                          child: const Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text('Pomodors',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 28,
+                                        color: Colors.black)),
+                                Text('0',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 58,
+                                        color: Colors.black)),
+                              ])),
+                    ),
+                  ],
+                ))
+          ],
+        ));
+  }
+}
